405edc2ce0caccabc8be1094f78a28c02244f30e
doshoimu <lecras@hotmail.fr>
2024-12-19 10:40:28 +0100

    commit final

--- assets/script.js
+++ assets/script.js
@@ -17,14 +17,41 @@ const slides = [
 	}
 ]
 
+let currentSlide = 0; // Index of current slide
+const bannerImg = document.querySelector('.banner-img'); // Main image element
+const bannerText = document.querySelector('#banner p'); // Main text element
 
 const arrowLeft = document.querySelector('.arrow_left');
 const arrowRight = document.querySelector('.arrow_right');
+const dots = document.querySelectorAll('.dot');
 
-arrowLeft.addEventListener("click", () => {
-	console.log('Flèche gauche cliquée')
-});
+function updateSlide(index) {
+	bannerImg.src = `./assets/images/slideshow/${slides[index].image}`; // Update main image
+    bannerText.innerHTML = slides[index].tagLine; // Update main text
+
+	dots.forEach(dot => dot.classList.remove('dot_selected'));
+	dots[index].classList.add('dot_selected');
+}
+
 arrowRight.addEventListener("click", () => {
+	if (currentSlide < slides.length - 1) {
+		currentSlide++;
+    } else {
+		currentSlide = 0; // Reset to the first slide when reaching the end
+	}
+	updateSlide(currentSlide);
 	console.log('Flèche droite cliquée')
 });
+
+arrowLeft.addEventListener("click", () => {
+	if (currentSlide > 0) {
+        currentSlide--;
+    } else {
+		currentSlide = slides.length - 1; // Reset to the last slide when reaching the beginning
+	}
+	updateSlide(currentSlide);
+	console.log('Flèche gauche cliquée')
+});
+
+updateSlide(currentSlide); // Initialize the slideshow with the first slide
 
